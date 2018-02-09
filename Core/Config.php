dfbf3fc25c4b3da53d98385c134b0e63a46bf398
jeulis <jeulis8@gmail.com>
2018-02-09 14:16:39 +0100

    variables Connexion BDD

--- Core/Config.php
+++ Core/Config.php
@@ -5,7 +5,7 @@ namespace Core;
 class Config{
     // For database settings
     const BDD_HOST      = 'localhost';
-    const BDD_NAME      = 'm2l_v3';
+    const BDD_NAME      = 'm2l_v6';
     const BDD_USER      = 'userweb';
     const BDD_PASSWORD  = 'pwweb';
 
--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $actionName=  \Core\Params::post('a',  \Core\Params::get('a','liste')).'Action';
 //
 // Prépare la connexion à la bdd
 //$connection = \Core\Connection::setup('connection','mysql:host='.BDD_HOST.';dbname='.BDD_NAME, BDD_USER, BDD_PASSWORD, array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
-$connection = \Core\Connection::setup('connection','mysql:host=localhost;dbname=m2l_v3', \Core\Config::BDD_USER, \Core\Config::BDD_PASSWORD, array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+$connection = \Core\Connection::setup('connection','mysql:host='.\Core\Config::BDD_HOST.';dbname='.\Core\Config::BDD_NAME, \Core\Config::BDD_USER, \Core\Config::BDD_PASSWORD, array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
 \Core\Object::setConnection($connection);
 
 // On fait appel au controlleur sélectionné avec les paramètres du POST et GET mis dans un seul tableau (array_merge)
