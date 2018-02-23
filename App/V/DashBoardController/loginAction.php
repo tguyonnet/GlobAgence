9c0904a77e5d2ca8312de6f974ff7b441dbffef1
jeulis <jeulis8@gmail.com>
2018-02-23 17:00:26 +0100

    Modif login form

--- App/V/DashBoardController/loginAction.php
+++ App/V/DashBoardController/loginAction.php
@@ -6,10 +6,11 @@
 
 //Display login form
 echo '<!-- Breadcrumbs-->
+      <div class="container">
       <div class="card card-login mx-auto mt-5">
-      <div class="card-header">Login</div>
+      <div class="card-header text-center text-white bg-dark">Formulaire de Connexion</div>
       <div class="card-body">
-        <form>
+        <form >
           <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
             <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email">
@@ -18,20 +19,30 @@ echo '<!-- Breadcrumbs-->
             <label for="exampleInputPassword1">Password</label>
             <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password">
           </div>
-          <div class="form-group">
-            <div class="form-check">
-              <label class="form-check-label">
-                <input class="form-check-input" type="checkbox"> Remember Password</label>
-            </div>
-          </div>
-          <a class="btn btn-primary btn-block" href="index.html">Login</a>
-        </form>
-        <div class="text-center">
-          <a class="d-block small mt-3" href="register.html">Register an Account</a>
-          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+          <div class="form-group text-center">
+          <input class="btn btn-dark" type="submit" value="Se connecter">
+          <a class="btn btn-info" href="register.html">Mot de passe oublié ?</a>
         </div>
+        </form>
+       
+      </div>
       </div>
     </div>';
+
+
+$timeTarget = 0.05; // 50 millisecondes
+
+$cost = 8;
+do {
+    $cost++;
+    $start = microtime(true);
+    echo password_hash("pwsio", PASSWORD_BCRYPT, ["cost" => $cost]);
+    $end = microtime(true);
+} while (($end - $start) < $timeTarget);
+
+
+
+
 
 
 
