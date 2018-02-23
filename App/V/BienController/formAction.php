19fff9f8cc4a60f485f92c0c7d433e848a4361f3
thomasPchd <thomas.pichaud85@gmail.com>
2018-02-23 17:19:45 +0100

    l

---
+++ App/V/BienController/formAction.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: usersio
+ * Date: 23/02/18
+ * Time: 17:18
+ */
+
+
+
+
+echo '<!-- Breadcrumbs-->
+      <div class="container">
+      <div class="card card-login mx-auto mt-5">
+      <div class="card-header text-center text-white bg-dark">Formulaire de Connexion</div>
+      <div class="card-body">
+        <form method="post">
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password">
+          </div>
+          <div class="form-group text-center">
+          <input class="btn btn-dark" type="submit" value="Se connecter">
+          <a class="btn btn-info" href="register.html">Mot de passe oublié ?</a>
+        </div>
+        </form>
+       
+      </div>
+      </div>
+    </div>';
