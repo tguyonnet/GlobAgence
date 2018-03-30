e1c6ca2e041c041b6b0751b94b798b9733c2c316
fPetitot <petito28fl@lycee-ndduroc.com>
2018-03-30 17:14:43 +0200

    Florian -- Liste des consultants

--- App/V/ConsultantController/listeAction.php
+++ App/V/ConsultantController/listeAction.php
@@ -12,7 +12,7 @@ echo '<div class="card mb-3">
         <div class="card-body">
           <div class="table-responsive">';
 echo '      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">';
-echo '      <thead><tr>Code utilisateur<th></th><th>Nom</th><th>Prenom</th><th>Zone</th><th>Nombre de biens en vente</th><th>Nombre de biens vendus</th><th>Chiffre d\'affaire</th></tr></thead><tboby>';
+echo '      <thead><th>Nom</th><th>Prenom</th><th>Zone</th><th>Nombre de biens en vente</th><th>Nombre de biens vendus</th><th>Chiffre d\'affaire</th></tr></thead><tboby>';
 
 // Contenu du tableau
 foreach($this->consultants as $consultant){
