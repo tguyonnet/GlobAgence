1a58e151db80ae51f4256353673298d72b2c34b4
fPetitot <petito28fl@lycee-ndduroc.com>
2018-03-30 16:18:48 +0200

    Liste des consultants

--- App/M/ConsultantModel.php
+++ App/M/ConsultantModel.php
@@ -10,7 +10,53 @@ namespace App\M;
 
 use Core\Object;
 
+
 class ConsultantModel extends Object
 {
+    protected static $_table = 'UTILISATEUR';
+
+    protected $id;
+    protected $nom;
+    protected $prenom;
+    protected $zone;
+    protected $nombreDeBiensEnVente;
+    protected $nombreDeBiensVendus;
+    protected $chiffreAffaire;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function getZone()
+    {
+        return $this->zone;
+    }
+
+    public function getNombreDeBiensEnVente()
+    {
+        return $this->nombreDeBiensEnVente;
+    }
+
+    public function getNombreDeBiensVendus()
+    {
+        return $this->nombreDeBiensVendus;
+    }
+
+    public function getChiffreAffaire()
+    {
+        return $this->chiffreAffaire;
+    }
+
 
 }
