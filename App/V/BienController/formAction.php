cb3a6403900b8b56178acd27fba11756a8a08c2d
jeulis <jeulis8@gmail.com>
2018-04-06 15:44:29 +0200

    Merge branch 'e0_v0' of /var/www/html/GlobAgence/GlobAgence with conflicts.

--- App/V/BienController/formAction.php
+++ App/V/BienController/formAction.php
@@ -6,6 +6,7 @@ echo '<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
       <div class="card card-login mx-auto mt-5">
       <div class="card-header text-center text-white bg-dark">Formulaire d\'ajout de bien</div>
       <div class="card-body" style="text-align: center;">
+      
         <form method="post">
           <div class="form-group">
             <label for="ville">Ville :</label>
