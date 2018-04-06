59e2d0b67e7d94d9447cc1aff4e33899769f27c0
thomasPchd <thomas.pichaud85@gmail.com>
2018-04-06 14:07:41 +0200

    poaẑkj

--- App/V/BienController/testBdd.php
+++ App/V/BienController/testBdd.php
@@ -5,8 +5,8 @@
  * Date: 30/03/18
  * Time: 14:05
  */
-function envoyerBien($connexion, $login, $superficieBien, $superficieTerrain, $prixHfai, $nbPiece, $numAppartement, $nombreChambre, $ville){
-    if(isset($_POST['VILLE']) && isset($_POST['SUPERFICIE_BIEN']) && isset($_POST['SUPERFICIE_TERRAIN']) && isset($_POST['PRIX_HFAI']) /**&& isset['PHOTOS']*/ && isset($_POST['NOMBRE_DE_PIECE']) && isset($_POST['NUM_APPARTEMENT']) && isset($_POST['NOMRE_DE_CHAMBRE'])){
+function envoyerBien($connexion, $login, $superficieBien, $superficieTerrain, $prixHfai, $nbPiece, $numAppartement, $nombreChambre, $ville, $codepostal){
+    if(isset($_POST['VILLE']) && ($_POST['CODEPOSTAL']) && isset($_POST['SUPERFICIE_BIEN']) && isset($_POST['SUPERFICIE_TERRAIN']) && isset($_POST['PRIX_HFAI']) /**&& isset['PHOTOS']*/ && isset($_POST['NOMBRE_DE_PIECE']) && isset($_POST['NUM_APPARTEMENT']) && isset($_POST['NOMRE_DE_CHAMBRE'])){
             $_POST['SUPERFICIE_BIEN']=$superficieBien;
             $_POST['SUPERFICIE_TERRAIN']=$superficieTerrain;
             $_POST['PRIX_HFAI']=$prixHfai;
@@ -14,6 +14,7 @@ function envoyerBien($connexion, $login, $superficieBien, $superficieTerrain, $p
             $_POST['NUM_APPARTEMENT']=$numAppartement;
             $_POST['NOMRE_DE_CHAMBRE']=$nombreChambre;
             $_POST['VILLE']=$ville;
+            $_POST['CODEPOSTAL']=$codepostal;
     }
 }
 $connexion = mysql_connect('172.21.4.195', 'usergrant', 'pwgrant');
@@ -21,4 +22,4 @@ mysql_select_db('GlobAgence_v1',$connexion) or die("Impossible d'ouvrir la base"
 $login = "$_SESSION[login]";
 
 
-$req=$db->query("INSERT INTO BIEN (SUPERFICIE_BIEN, SUPERFICIE_TERRAIN, PRIX_HFAI, NOMBRE_DE_PIECE, NUM_APPARTEMENT, NOMBRE_DE_CHAMBRE) VALUES ('$superficieBien','$superficieTerrain','$prixHfai','$nbPiece','$numAppartement','$nombreChambre')");
+$req=$db->query("INSERT INTO BIEN (SUPERFICIE_BIEN, SUPERFICIE_TERRAIN, PRIX_HFAI, NOMBRE_DE_PIECE, NUM_APPARTEMENT, NOMBRE_DE_CHAMBRE, VILLE) VALUES ('$superficieBien','$superficieTerrain','$prixHfai','$nbPiece','$numAppartement','$nombreChambre','$ville')");
