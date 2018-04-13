691f6003ed2e2f167034e5c9538a382ced176a06
jeulis <jeulis8@gmail.com>
2018-04-13 14:57:04 +0200

    bapt

---
+++ App/V/ConsultantController/formAction.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: usersio
+ * Date: 13/04/18
+ * Time: 14:11
+ */
+
+
+echo '<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    <div class="container">
+      <div class="card card-login mx-auto mt-5">
+      <div class="card-header text-center text-white bg-dark">Formulaire d\'ajout de consultant</div>
+      <div class="card-body text-center">
+        <form method="post" action="?c=Consultant&a=queryForm">
+          <div class="form-group">
+            <label for="nom">Nom :</label>
+            <input type="text" name="nom">
+          </div>  
+          <div class="form-group">
+            <label for="prenom">Prenom :</label>
+            <input type="text" name="codepostal">
+          </div>
+          <div class="form-group">
+            <label for="dateNaissance">Date de naissance :</label>
+            <input type="text" name="dateNaissance">
+          </div>
+          <div class="form-group">
+            <label for="email">Email :</label>
+            <input type="text" name="email">
+          </div>
+          <div class="form-group">
+            <label for="telephone">Numéro de téléphone :</label>
+            <input type="text" name="telephone">
+          </div>
+          <div class="form-group">
+          <label for="rue">Rue :</label>
+            <input type="text" name="telephone">
+         </div>
+         <div>
+         <div class="form-group">
+         <label for="ville">Ville :</label>
+            <input type="text" name="ville">
+          </div>
+          <div>
+          <div class="form-group">
+          <label for="codePostal">Code Postal :</label>
+            <input type="text" name="codePostal">
+          </div>
+         <div class="form-group">
+            <label for="valider">Valider :</label>
+            <input type="submit" name="valider">
+          </div>
+        </form>
+      </div>
+      </div>
+    </div>';
