49305ff2b36c4d30040a1ec199e895b8cbf08abc
fPetitot <36079461+fPetitot@users.noreply.github.com>
2018-04-20 13:48:23 +0200

    Update ConsultantController.php

--- App/C/ConsultantController.php
+++ App/C/ConsultantController.php
@@ -8,13 +8,16 @@
 
 namespace App\C;
 
+use App\M\ConsultantModel;
 use App\M\UtilisateurModel;
+use App\M\ZoneModel;
+
 
 class ConsultantController extends \Core\Controller {
 
 
     static public function listeAction() {
-        self::$_view->consultants = UtilisateurModel::getAll();
+        self::$_view->consultants = ConsultantModel::getAll();
         self::$_view->title = 'Liste des consultants';
     }
 
